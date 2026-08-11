a501b2e272fc46e12bbe606e294bdfa35a518dbd
Deepanshi Gupta <deepanshigupta104@gmail.com>
2026-08-11 21:39:54 +0530

    Type Function

---
+++ type.py
@@ -0,0 +1,21 @@
+"""
+a = "31.2"
+b = float(a)
+t = type(b)
+print (t) 
+"""
+
+#  conversion
+a = "32.1"
+b = float(a)
+t = type(b)
+print (t)
+
+
+# Input Function
+a = int(input("Enter Number 1 :"))
+b = int(input("Enter Number 2 :"))
+
+print("Number a is : " , a)
+print("Number b is : " , b)
+print("Sum is  : " , a+b)
