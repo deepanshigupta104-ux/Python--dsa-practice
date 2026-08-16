88aa45c5cf4e83d7b796d1f8856183091c6c5e0b
Deepanshi Gupta <deepanshigupta104@gmail.com>
2026-08-16 12:23:27 +0530

    Second Practice

---
+++ pythonps/sec.py
@@ -0,0 +1,24 @@
+# write a program to add Two Numbers
+"""a =int(input("Enter number 1: " ))
+b=int(input("Enter Number 2: " ))
+c = a + b
+print("Sum is: ", c)"""
+
+# Write a program to find a remainder when a number is divided by z
+"""a = 15
+b = 5
+print("Remainder when a divides by b is :" , a%b )"""
+
+#Check the type of variable assigned using input() function
+"""a= input("Enter Number 1 :" , )
+print(type(a))"""
+
+#Use Comparision Operator to find out whether a given Variable a is Greater than 'B' Oe not Take A = 35 amd B = 80
+"""A =int(input("Enter number 1: " ))
+B =int(input("Enter number 2: " ))
+print ("A is Greater Than B is " ,A>B)"""
+
+# Write a program to find a average of a Two Number Entered By user
+A=int(input("Enter number 1: " ))
+
+print ("Square of  Number is :", A*A)
