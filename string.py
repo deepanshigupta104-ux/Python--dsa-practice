4fb09e48bda1efbf02afb1da69036c9b52d6168e
Deepanshi Gupta <deepanshigupta104@gmail.com>
2026-08-22 19:38:27 +0530

    String

---
+++ string.py
@@ -0,0 +1,26 @@
+A = "Deepa"
+print(A[0:3])
+print(A[-5:-1])
+print(A[:])
+print(A[:4])
+print(A[0:])
+#ADVANCED SLICING 
+name = "Amazing"
+print(name[1: 6: 2])
+
+# STRING FUNCTIONS
+Name = "deepanshi"
+print(len(Name))
+print(Name.endswith("shi"))
+print(Name.startswith("Dee"))
+print(Name.capitalize())
+
+S = " deepanshi gupta "
+print(S.title())
+print(S.rstrip())
+print(S.count("s"))
+print(S.find("shi"))
+
+#Escape Sequence 
+a = "deep is \"btech \""
+print(a)
